7d095d4dc91787f7d5242e54ffb3489c793ef0d5
Deyvison Borges <deyvisonsilvaborges@gmail.com>
2021-09-02 22:48:38 -0300

    feat: controllers - get, post

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/controller/MateriaController.java
@@ -0,0 +1,43 @@
+package com.rasmoo.cliente.escola.gradecurricular.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rasmoo.cliente.escola.gradecurricular.entity.MateriaEntity;
+import com.rasmoo.cliente.escola.gradecurricular.repository.IMateriaRepository;
+
+@RestController
+@RequestMapping("/materia") // urn
+public class MateriaController {
+	@Autowired
+	private IMateriaRepository materiaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<MateriaEntity>> listarMaterias() {
+		return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<MateriaEntity> consularMateria(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(materiaRepository.findById(id).get());
+	}
+
+	@PostMapping
+	public ResponseEntity<Boolean> cadastrarMateria(@RequestBody MateriaEntity materia) {
+		try {
+			this.materiaRepository.save(materia);
+			return ResponseEntity.status(HttpStatus.OK).body(true);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.OK).body(false);
+		}
+	}
+}
